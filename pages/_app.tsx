b8f326aef8db72edc5f9d6441381b4071f1fc82f
Mahmod <mahmodghnaj1@gmail.com>
2023-04-18 18:39:23 +0300

    add title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Mahmod Ghnaj</title>
+        <meta name="title" content="Mahmod Ghnaj"></meta>
+        <meta
+          name="description"
+          content="Mahmod Ghnaj Software Engineer für ReactJS, TypeScript, NodeJS"
+        ></meta>
+        <link rel="icon" href="/logo.jpeg" type="image/icon type" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
